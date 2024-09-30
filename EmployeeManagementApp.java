7abf3ec743b6971c253c93edee704f2f9ddaa38d
Mohammed Qutu <q2mohammed@gmail.com>
2024-09-30 14:11:34 -0500

    Create EmployeeManagementApp.java

---
+++ EmployeeManagementApp.java
@@ -0,0 +1,9 @@
+public class EmployeeManagementApp {
+
+    public static void main(String[] args) {
+        Person[] people = new Person[4];
+
+        people[0] = new Person("Bob", "123-456-7890", "alice@example.com");
+        people[1] = new Person("Jeff", "214-399-2034", "bob@example.com");
+
+        people[2] = new Employee("Carl", "333-444-5555", "Carl@gmail.com", "1234-5678
